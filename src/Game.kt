d0942cebb18088e1ca7efc8444276ec35068565e
NeimadTL <damientalbot26@gmail.com>
2021-11-01 20:21:05 +0100

    Chapter 4: functions
    I've learnt about about functions and built-in Kotlin's function
    syntax like single-expression function:, named function arguments
    and default arguments.

--- src/Game.kt
+++ src/Game.kt
@@ -5,10 +5,27 @@ fun main(args: Array<String>){
     val isImmortal = false
 
     // Aura
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
 
-    val healthStatus = when(healthPoints) {
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+
+    // Player status
+    val inebriationLevel = castFireBall(3)
+    val inebriationStatus = inebriationStatus(inebriationLevel)
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus, inebriationStatus)
+
+}
+
+private fun printPlayerStatus(auraColor: String, isBlessed: Boolean, name: String, healthStatus: String, inebriationStatus: String) {
+    println("(Aura: $auraColor) " + "(Blessed: ${if (isBlessed) "YES" else "NO"}) " + "(Inebriation: $inebriationStatus)")
+    println("$name $healthStatus")
+}
+
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean) =
+    if (isBlessed && healthPoints > 50 || isImmortal)  "GREEN" else "NONE"
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
+     when (healthPoints) {
         100 -> "is in excellent condition!"
         in 90..99 -> "has a few scratches."
         in 75..89 -> if (isBlessed) {
@@ -20,7 +37,21 @@ fun main(args: Array<String>){
         else -> "is in awful condition!"
     }
 
-    // Player status
-    println("(Aura: $auraColor) " + "(Blessed: ${if (isBlessed) "YES" else "NO"})")
-    println("$name $healthStatus")
+private fun castFireBall(numFireballs: Int = 2): Int {
+    var inebriation = 0
+    if (numFireballs > 2 ) {
+        inebriation = (Math.random() * 50).toInt()
+        println("A glass of Fireball springs into existence. (x$numFireballs)")
+    }
+    return inebriation
 }
+
+private fun inebriationStatus(inebriation: Int) =
+    when (inebriation) {
+        in 1..10 -> "tipsy"
+        in 11..20 -> "sloshed"
+        in 21..30 -> "soused"
+        in 31..40 -> "stewed"
+        in 41..50 -> "t0aSt3d"
+        else -> "Game Over"
+    }
