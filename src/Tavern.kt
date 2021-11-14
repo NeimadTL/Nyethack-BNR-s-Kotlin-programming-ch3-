9c1d0f3055f8b3030ffaa2f1cba6ebae22edbf90
NeimadTL <damientalbot26@gmail.com>
2021-11-14 15:36:36 +0100

    Add chapter 7: Strings
    This chapter showcases the basic of how to use String in Kotlin.
    It showcase methods like indexOf, replace, lowercase, substring
    and split.

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,3 +1,5 @@
+const val TAVERN_NAME = "Taernyl's Folly"
+
 fun main(args: Array<String>) {
     // 3 options for dealing with nullable value
 
@@ -20,6 +22,7 @@ fun main(args: Array<String>) {
     //println(beverage)
 
     // 3) basic check and/or default value
+    /*
     var beverage = readLine()
     // beverage = null
     if (beverage!= null) {
@@ -29,6 +32,37 @@ fun main(args: Array<String>) {
     }
     val beverageServed: String = beverage ?: "Buttered Ale"
     println(beverageServed)
+    */
 
-
+    placeOrder("shandy,Dragon's Breath,5.91")
+    placeOrder("elixir,Shirley's Temple,4.12")
 }
+
+fun placeOrder(menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster =  TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("Madrigal speaks with $tavernMaster about their order.")
+
+    val (type, name, price) = menuData.split(',')
+    val message = "Madrigal buys a $name ($type) for $price."
+    println(message)
+
+    val phrase = if (name == "Dragon's Breath") {
+        "Madrigal exclaims ${toDragonSpeak("Ah, delicious $name!")}"
+    } else {
+        "Madrigal says: Thanks for the $name"
+    }
+    println(phrase)
+}
+
+private fun toDragonSpeak(phrase: String) =
+    phrase.lowercase().replace(Regex("[aeiou]")){
+        when (it.value){
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "o" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        }
+    }
